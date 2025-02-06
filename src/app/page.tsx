b227532ad1174a89df4be409e0c84201d0a2cfed
expl0r3rgu1 <salvatore.addimando@startegois.com>
2025-02-06 15:14:36 +0100

    Shadcn init + first button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 'use client'
 
+import { Button } from '@/components/ui/button'
 import { RootContext } from '@/contexts/root-context'
 import { useContext } from 'react'
 
 export default function Home() {
   const { currentUser } = useContext(RootContext)
-  return <h1>Hello World! {currentUser}</h1>
+  return (
+    <div className="flex flex-row justify-center gap-2">
+      <h1>Hello World! {currentUser}</h1>
+      <Button>Click me</Button>
+    </div>
+  )
 }
