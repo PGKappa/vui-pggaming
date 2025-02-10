7451570de4c829a216e96426a01cff7c35f4cc46
expl0r3rgu1 <salvatore.addimando@startegois.com>
2025-02-10 16:12:48 +0100

    Add Leaderboard, LiveRoundScores, and UpcomingRoundCard components with loading state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,35 @@
 'use client'
 
 import BettingSlip from '@/components/betting-slip'
-import RoundLiveScores from '@/components/round-live-scores'
+import Leaderboard from '@/components/leaderboard'
+import LiveRoundScores from '@/components/live-round-scores'
+import LoadingSpinner from '@/components/loading-spinner'
+import UpcomingRoundCard from '@/components/upcoming-round-card'
 import VideoStreamCard from '@/components/video-stream-card'
+import { RootContext } from '@/contexts/root-context'
+import { useContext } from 'react'
 
 export default function Home() {
+  const { upcomingRounds } = useContext(RootContext)
   return (
     <div className="grid grid-cols-12 justify-center gap-2 border border-black px-10 py-4">
-      <div className="col-span-6 border border-black p-2">
+      <div className="col-span-6 space-y-3 border border-black p-2">
         <VideoStreamCard />
+        {upcomingRounds ? (
+          <ol className="space-y-2">
+            {upcomingRounds.map((round) => (
+              <li key={round.number}>
+                <UpcomingRoundCard round={round} />
+              </li>
+            ))}
+          </ol>
+        ) : (
+          <LoadingSpinner />
+        )}
       </div>
-      <div className="col-span-3 border border-black p-2">
-        <RoundLiveScores />
+      <div className="col-span-3 space-y-3 border border-black p-2">
+        <LiveRoundScores />
+        <Leaderboard />
       </div>
       <div className="col-span-3 border border-black p-2">
         <BettingSlip />
