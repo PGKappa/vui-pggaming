039b885cf0683266426e1ebba76301c7c11e46cf
expl0r3rgu1 <salvatore.addimando@startegois.com>
2025-02-10 16:11:31 +0100

    Add VideoStreamCard component

---
+++ src/components/video-stream-card.tsx
@@ -0,0 +1,9 @@
+import { Card, CardContent } from './ui/card'
+
+export default function VideoStreamCard() {
+  return (
+    <Card>
+      <CardContent>VideoStreamCard</CardContent>
+    </Card>
+  )
+}
