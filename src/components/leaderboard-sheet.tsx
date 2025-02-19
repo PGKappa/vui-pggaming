f245a93a555164687d781ae28e39fac195e89128
expl0r3rgu1 <salvatore.addimando@startegois.com>
2025-02-19 10:17:29 +0100

    Add BettingSlipSheet and LeaderboardSheet components

---
+++ src/components/leaderboard-sheet.tsx
@@ -0,0 +1,25 @@
+import Leaderboard from './leaderboard'
+import { Button } from './ui/button'
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from './ui/sheet'
+
+export default function LeaderboardSheet() {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button size="lg">Classifica</Button>
+      </SheetTrigger>
+      <SheetContent side="top" className="p-0">
+        <SheetHeader>
+          <SheetTitle>Classifica</SheetTitle>
+        </SheetHeader>
+        <Leaderboard />
+      </SheetContent>
+    </Sheet>
+  )
+}
