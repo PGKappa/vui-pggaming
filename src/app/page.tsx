ea401dff1173912d41f9bf86427206d453758dda
expl0r3rgu1 <salvatore.addimando@startegois.com>
2025-02-19 14:54:16 +0100

    Refactor Home component layout for improved structure and readability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,39 +15,41 @@ export default function Home() {
   const { upcomingRounds, liveRound } = useContext(RootContext)
 
   return (
-    <div className="container grid grid-cols-1 justify-center gap-3 py-4 lg:grid-cols-4">
-      <div className="space-y-3 lg:col-span-2">
-        <VideoStreamCard streamUrl={liveRound?.streamUrl} />
-        {upcomingRounds ? (
-          <ol className="space-y-2">
-            {upcomingRounds.map((round) => (
-              <li key={round.number}>
-                <UpcomingRoundCard round={round} />
-              </li>
-            ))}
-          </ol>
-        ) : (
-          <div className="flex justify-center">
-            <LoadingSpinner />
-          </div>
-        )}
-      </div>
-      <div className="space-y-3 lg:col-span-1">
-        {/* <LiveRoundScores /> */}
-        <LiveRoundStatistics />
-        <div className="hidden lg:block">
-          <Leaderboard />
+    <>
+      <div className="container grid grid-cols-1 justify-center gap-3 py-4 lg:grid-cols-4">
+        <div className="space-y-3 lg:col-span-2">
+          <VideoStreamCard streamUrl={liveRound?.streamUrl} />
+          {upcomingRounds ? (
+            <ol className="space-y-2">
+              {upcomingRounds.map((round) => (
+                <li key={round.number}>
+                  <UpcomingRoundCard round={round} />
+                </li>
+              ))}
+            </ol>
+          ) : (
+            <div className="flex justify-center">
+              <LoadingSpinner />
+            </div>
+          )}
         </div>
-      </div>
-      <div className="lg:col-span-1">
-        <div className="hidden lg:block">
-          <BettingSlip />
+        <div className="space-y-3 lg:col-span-1">
+          {/* <LiveRoundScores /> */}
+          <LiveRoundStatistics />
+          <div className="hidden lg:block">
+            <Leaderboard />
+          </div>
+        </div>
+        <div className="lg:col-span-1">
+          <div className="hidden lg:block">
+            <BettingSlip />
+          </div>
         </div>
       </div>
       <div className="fixed bottom-0 flex w-full justify-center gap-2 lg:hidden">
         <LeaderboardSheet />
         <BettingSlipSheet />
       </div>
-    </div>
+    </>
   )
 }
