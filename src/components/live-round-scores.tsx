a640c73d41470cfed2981cf07fc7665ba1714f87
AndrewChop <amarantoandreagmail.com>
2025-03-04 10:37:13 +0100

    Refactor LiveRoundScores using Table component

--- src/components/live-round-scores.tsx
+++ src/components/live-round-scores.tsx
@@ -1,10 +1,16 @@
 'use client'
 import { RootContext } from '@/contexts/root-context'
-import { Fragment, useContext } from 'react'
-import { Badge } from './ui/badge'
+import { useContext } from 'react'
 import { Card, CardContent, CardHeader, CardTitle } from './ui/card'
-import { Separator } from './ui/separator'
 import LoadingSpinner from './loading-spinner'
+import {
+  Table,
+  TableHeader,
+  TableBody,
+  TableRow,
+  TableCell,
+} from './ui/table'
+import { Badge } from './ui/badge'
 
 export default function LiveRoundScores() {
   const { liveRound } = useContext(RootContext)
@@ -25,27 +31,39 @@ export default function LiveRoundScores() {
   return (
     <Card>
       <CardHeader>
-        <CardTitle>
+        <CardTitle className="h-5">
           {liveRound.name} Round {liveRound.number}
         </CardTitle>
       </CardHeader>
       <CardContent>
-        <ul className="space-y-1">
-          {liveRound.scores.map((score, index) => (
-            <Fragment key={index}>
-              <li className="flex items-center gap-2">
-                <Badge>LIVE</Badge>
-                <span className="flex-1 text-sm font-medium leading-none">
-                  {score.team1} - {score.team2}
-                </span>
-                <span className="text-sm font-medium leading-none">
+        <Table>
+          <TableHeader />
+          {/* <TableHeader>
+            <TableRow>
+              <TableHead className="text-center">Stato</TableHead>
+              <TableHead className="text-center">Partita</TableHead>
+              <TableHead className="text-center">Punteggio</TableHead>
+            </TableRow>
+          </TableHeader> */}
+          <TableBody className="border-b border-t border-card-foreground">
+            {liveRound.scores.map((score, index) => (
+              <TableRow
+                key={index}
+                className="border-b border-t border-card-foreground"
+              >
+                <TableCell className="flex flex-row items-center gap-2">
+                  <Badge>LIVE</Badge>
+                  <span className="text-nowrap font-bold">
+                    {score.team1} - {score.team2}
+                  </span>
+                </TableCell>
+                <TableCell className="text-left font-bold">
                   {score.score1} - {score.score2}
-                </span>
-              </li>
-              {index < liveRound.scores.length - 1 && <Separator />}
-            </Fragment>
-          ))}
-        </ul>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </CardContent>
     </Card>
   )
