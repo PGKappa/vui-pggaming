ebf10da2abae79585ac0e3d85c5726344e9a4828
AndrewChop <amarantoandreagmail.com>
2025-03-13 15:46:50 +0100

    Update betting slip sheet with translations

--- src/components/betting-slip-sheet.tsx
+++ src/components/betting-slip-sheet.tsx
@@ -1,22 +1,18 @@
 import { VisuallyHidden } from '@radix-ui/react-visually-hidden'
 import BettingSlip from './betting-slip'
 import { Button } from './ui/button'
-import {
-  Sheet,
-  SheetContent,
-  SheetTitle,
-  SheetTrigger
-} from './ui/sheet'
+import { Sheet, SheetContent, SheetTitle, SheetTrigger } from './ui/sheet'
+import { t } from 'i18next'
 
 export default function BettingSlipSheet() {
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <Button size="lg">Schedina</Button>
+        <Button size="lg">{t('bet_slip')}</Button>
       </SheetTrigger>
       <SheetContent side="top" className="p-0">
         <VisuallyHidden>
-          <SheetTitle>Schedina</SheetTitle>
+          <SheetTitle>{t('bet_slip')}</SheetTitle>
         </VisuallyHidden>
         <BettingSlip />
       </SheetContent>
