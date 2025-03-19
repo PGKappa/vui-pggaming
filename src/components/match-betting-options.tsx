fc879a4faeb392d9905b7a8b6e2e65e12edfd0a4
AndrewChop <amarantoandreagmail.com>
2025-03-19 11:01:48 +0100

    Remove unused code and refactor MatchBettingOptions component

--- src/components/match-betting-options.tsx
+++ src/components/match-betting-options.tsx
@@ -1,11 +1,9 @@
 'use client'
 
-import { BetsContext } from '@/contexts/bets-context'
-import { Market, Selection } from '@/lib/types'
+import { Market } from '@/lib/types'
 import { format } from 'date-fns'
 import { t } from 'i18next'
 import { ChevronsLeftIcon } from 'lucide-react'
-import { useContext } from 'react'
 import BetEntryToggle from './bet-entry-toggle'
 import {
   Accordion,
@@ -26,21 +24,6 @@ export default function MatchBettingOptions(props: {
   markets: Market[]
   close: () => void
 }) {
-  const { addBet, betEntries, removeBet } = useContext(BetsContext)
-
-  const isBetSelected = (
-    marketName: string,
-    option: Selection,
-    teams: string,
-  ) => {
-    return betEntries.some(
-      (entry) =>
-        entry.market === marketName &&
-        entry.bet.teams === teams &&
-        entry.bet.option.outcome === option.outcome,
-    )
-  }
-
   return (
     <div className="flex w-full flex-col gap-2">
       <div className="flex flex-row items-center justify-between">
