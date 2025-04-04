66147db4685b5a78d7b20217106710e58802644e
AndrewChop <amarantoandreagmail.com>
2025-04-04 16:31:08 +0200

    Update Toggle component styles for improved visual consistency and accessibility

--- src/components/ui/toggle.tsx
+++ src/components/ui/toggle.tsx
@@ -1,17 +1,17 @@
 'use client'
 
-import * as React from 'react'
 import * as TogglePrimitive from '@radix-ui/react-toggle'
 import { cva, type VariantProps } from 'class-variance-authority'
+import * as React from 'react'
 
 import { cn } from '@/lib/utils'
 
 const toggleVariants = cva(
-  'inline-flex items-center justify-center gap-2 rounded-md text-sm font-medium transition-colors hover:bg-muted focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-bet data-[state=on]:text-bet-foreground [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+  'border border-betEntry-border inline-flex items-center justify-center gap-2 rounded-md text-sm font-medium transition-colors hover:bg-muted focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-bet data-[state=on]:text-bet-foreground [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground shadow hover:bg-accent/70',
+        default: 'bg-betEntry text-betEntry-foreground shadow hover:bg-accent/70 hover:border-accent',
         outline:
           'border border-input bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground',
       },
