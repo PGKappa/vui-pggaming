4baf74112fd094dc8a5b3eb863b567b00da1566a
AndrewChop <amarantoandreagmail.com>
2025-04-10 10:11:28 +0200

    Remove unused getCountdown function to streamline utility code

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,20 +7,6 @@ export function cn(...inputs: ClassValue[]) {
 
 export const BASE_API_URL = process.env.NEXT_PUBLIC_BASE_API_URL
 
-export function getCountdown(targetTime: Date) {
-  const total = targetTime.getTime() - Date.now()
-
-  const minutes = Math.floor((total / 1000 / 60) % 60)
-  const seconds = Math.floor((total / 1000) % 60)
-
-  return {
-    total,
-    formatted: `${minutes.toString().padStart(2, '0')}:${seconds
-      .toString()
-      .padStart(2, '0')}`,
-  }
-}
-
 export function getTimeDistanceFromNow(targetTime: Date) {
   const diff = targetTime.getTime() - Date.now()
 
