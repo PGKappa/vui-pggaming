21ab120d8f56094f012bb6d65ce1ec14fe8cc13c
AndrewChop <amarantoandreagmail.com>
2025-05-12 12:41:02 +0200

    refactor: improve styling and layout of FastBet component for better readability

--- src/retail-components/fast-bet.tsx
+++ src/retail-components/fast-bet.tsx
@@ -1,12 +1,19 @@
 import { Input } from '@/retail-components/ui/input'
 import { Button } from '@/retail-components/ui/button'
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '@/retail-components/ui/dialog'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/retail-components/ui/dialog'
 
 function CodeList() {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button variant="betNow">
+        <Button className="font-bold text-sm bg-bet text-bet-foreground">
           Code List
         </Button>
       </DialogTrigger>
@@ -25,14 +32,14 @@ function CodeList() {
 
 export default function FastBet() {
   return (
-    <div className="flex w-full flex-col bg-accent px-2 py-4 gap-2">
+    <div className="flex w-full flex-col gap-2 bg-accent px-2 py-3">
       <div className="flex flex-row items-center justify-between">
-        <span className="text-bet-foreground font-bold">Fastbet</span>
+        <span className="font-bold text-bet-foreground">Fastbet</span>
         <CodeList />
       </div>
-      <div className='flex flex-row items-center gap-1'>
-        <Input className='h-10 w-1/4' placeholder='Code' />
-        <Input className='h-10 w-3/4' placeholder='Selection' />
+      <div className="flex flex-row items-center gap-1">
+        <Input className="h-10 w-1/4" placeholder="Code" />
+        <Input className="h-10 w-3/4" placeholder="Selection" />
       </div>
     </div>
   )
