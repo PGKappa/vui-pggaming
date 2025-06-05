de243e15356c2ad22d2ae466d4eb4cb34ccaac9d
AndrewChop <amarantoandreagmail.com>
2025-06-05 10:32:11 +0200

    feat: set default state of open in Leaderboard component to true

--- src/retail-components/leaderboard.tsx
+++ src/retail-components/leaderboard.tsx
@@ -10,7 +10,7 @@ import { ChevronDown, ChevronUp } from 'lucide-react'
 
 export default function Leaderboard() {
   const { teamRankings } = useContext(RootContext)
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(true)
 
   return (
     <Card>
