af364a049a004de1dd7154dca1a41c37f35900e4
expl0r3rgu1 <salvatore.addimando@startegois.com>
2025-06-05 14:42:47 +0200

    refactor: improve navbar layout and styling with flex containers and consistent button widths

--- src/retail-components/navbar.tsx
+++ src/retail-components/navbar.tsx
@@ -1,3 +1,4 @@
+import { cn } from '@/retail-lib/utils'
 import { Info } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -5,43 +6,47 @@ import { buttonVariants } from './ui/button'
 
 export default function Navbar() {
   return (
-    <nav className="grid w-full grid-cols-12 items-center justify-between gap-4 bg-accent p-3">
-      <div className="col-span-2 flex justify-center">
-        <span className="whitespace-nowrap text-[16px] font-semibold text-background">
-          Select the category:
-        </span>
-      </div>
+    <nav className="flex w-full flex-row items-center justify-start bg-accent p-3">
+      <span className="whitespace-nowrap pl-14 text-center text-[16px] font-semibold text-background">
+        Select the category:
+      </span>
 
-      <div className='col-span-7 flex items-center gap-4'>
+      <div className="flex flex-row items-center gap-4 pl-[68px]">
         <Link
           href="/dogs-horses"
-          className={buttonVariants({
-            variant: 'navbar',
-            size: 'lg',
-          })}
+          className={cn(
+            buttonVariants({
+              variant: 'navbar',
+              size: 'lg',
+            }),
+            'flex w-32 flex-row items-center',
+          )}
         >
           <Image
             src="/dog-image.png"
             alt="Dogs"
             width={40}
             height={20}
-            className="h-5 w-full object-contain"
+            className="object-contain"
           />
           <Image
             src="/horse-image.png"
             alt="Horses"
             width={40}
             height={20}
-            className="h-6 w-full object-contain"
+            className="object-contain"
           />
         </Link>
 
         <Link
           href="/dogs"
-          className={buttonVariants({
-            variant: 'navbar',
-            size: 'lg',
-          })}
+          className={cn(
+            buttonVariants({
+              variant: 'navbar',
+              size: 'lg',
+            }),
+            'flex w-32 flex-row items-center',
+          )}
         >
           <Image
             src="/dog-image.png"
@@ -55,10 +60,13 @@ export default function Navbar() {
 
         <Link
           href="/horses"
-          className={buttonVariants({
-            variant: 'navbar',
-            size: 'lg',
-          })}
+          className={cn(
+            buttonVariants({
+              variant: 'navbar',
+              size: 'lg',
+            }),
+            'flex w-32 flex-row items-center',
+          )}
         >
           <Image
             src="/horse-image.png"
@@ -67,7 +75,7 @@ export default function Navbar() {
             height={20}
             className="object-contain"
           />
-          <span className="text-[16px] font-bold">Ch3</span>
+          <span className="text-[16px] font-bold">Ch2</span>
         </Link>
 
         <Link
@@ -88,7 +96,7 @@ export default function Navbar() {
         </Link>
       </div>
 
-      <div className="col-span-3 flex justify-end gap-4">
+      <div className="flex w-full justify-end gap-4">
         <Link
           href="/retail/ticket-list"
           className={buttonVariants({
