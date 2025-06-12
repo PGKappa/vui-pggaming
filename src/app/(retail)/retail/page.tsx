36063039bdc2538e5beb3e27b706d55382198afb
AndrewChop <amarantoandreagmail.com>
2025-06-12 16:22:45 +0200

    fix: adjust layout spacing in retail page for improved UI consistency

--- src/app/(retail)/retail/page.tsx
+++ src/app/(retail)/retail/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
   return (
     <div className="flex h-full overflow-hidden">
       {/* First column */}
-      <div className="flex h-[942px] w-[263px] flex-col items-center justify-between gap-4 overflow-hidden">
+      <div className="flex h-[942px] w-[263px] flex-col items-center justify-between gap-2 overflow-hidden">
         <UpcomingRoundsCard
           rounds={upcomingRounds}
           selectedRound={selectedRound}
@@ -93,7 +93,7 @@ export default function Home() {
       </div>
 
       {/*RIGHT COLUMN - Betting slip*/}
-      <div className="ml-2 h-[942px] w-[375px] overflow-y-auto bg-background text-foreground">
+      <div className="ml-2 h-[942px] w-[382px] overflow-y-auto bg-background text-foreground">
         <BettingSlip />
       </div>
     </div>
