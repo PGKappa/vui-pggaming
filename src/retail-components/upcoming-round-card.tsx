1aeb7b02d24ee4d98df0243614974cc2a22ee025
AndrewChop <amarantoandreagmail.com>
2025-07-08 13:16:25 +0200

    chore: update dependencies to latest versions for improved performance

--- src/retail-components/upcoming-round-card.tsx
+++ src/retail-components/upcoming-round-card.tsx
@@ -10,8 +10,7 @@ import {
 } from '@/retail-components/ui/table'
 import { Market, UpcomingRound } from '@/retail-lib/types'
 import { ChevronRight } from 'lucide-react'
-import Image from 'next/image'
-import { Dispatch, SetStateAction } from 'react'
+import { Dispatch, SetStateAction, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import BetEntryToggle from './bet-entry-toggle'
 
@@ -32,61 +31,86 @@ export default function UpcomingRoundCard(props: {
     >
   >
 }) {
-  const { t, i18n } = useTranslation()
+  const { t } = useTranslation()
 
   const today = new Date()
   const tomorrow = new Date()
   tomorrow.setDate(today.getDate() + 1)
 
-  const firstMatchStart = props.round.mag_event[0]?.startTime
-  const formattedFirstMatchStart = firstMatchStart
-    ? new Date(firstMatchStart).toLocaleTimeString(i18n.language, {
-        hour: '2-digit',
-        minute: '2-digit',
-      })
-    : ''
+  const marketTabs: { name: string; markets: Market[] }[] = [
+    {
+      name: 'Principali',
+      markets: props.round.mag_event[0].markets.market.filter((market) =>
+        [
+          'Esito finale 1X2',
+          'Doppia Chance',
+          'Under\/Over 2.5',
+          'Gol no gol',
+        ].includes(market.name.trim()),
+      ),
+    },
+    {
+      name: 'Under/Over',
+      markets: props.round.mag_event[0].markets.market.filter((market) =>
+      [
+        'Under/Over 1.5',
+        'Under/Over 2.5',
+        'Under/Over 3.5',
+        'Under/Over 4.5',
+      ].includes(market.name.trim()),
+      ),
+    }
+  ]
+
+  const [selectedTab, setSelectedTab] = useState(marketTabs[0].name)
+
+  function getFloatFromString(text: string): number | undefined {
+    const match = text.match(/[-+]?\d*\.\d+([eE][-+]?\d+)?/);
+    return match ? parseFloat(match[0]) : undefined;
+  }
 
   return (
     <Card className="border-b border-t border-card-foreground">
-      <CardHeader className="flex h-16 flex-row items-center justify-between px-5">
-        <div className="flex flex-row items-center gap-2">
-          <Image
-            src="/icon-calcio.png"
-            alt="Calcio"
-            width={40}
-            height={20}
-            className="size-9 object-contain"
-          />
-
-          <span className="text-[24px] font-bold">
-            {props.round.scheduleName} {t('round')} {props.round.scheduleId}
-          </span>
-        </div>
-
-        <span className="text-[22px]">{formattedFirstMatchStart}</span>
+      <CardHeader className="flex h-16 flex-row items-center justify-start bg-accent">
+        {marketTabs.map((tab, index) => (
+          <Button
+            key={index}
+            variant="navbarSelected"
+            className="h-full w-[150px] text-[20px] font-semibold"
+            onClick={() => {
+              setSelectedTab(tab.name)
+            }}
+          >
+            {tab.name}
+          </Button>
+        ))}
       </CardHeader>
       <CardContent className="px-0">
         <Table>
           <TableHeader className="h-11 bg-card-header text-[20px] text-card-header-foreground">
             <TableRow className="border-card-foreground transition-none">
-              <TableHead className='w-[225px]'></TableHead>
-              <TableHead className="text-center font-bold">1</TableHead>
-              <TableHead className="text-center font-bold">X</TableHead>
-              <TableHead className="text-center font-bold">2</TableHead>
-              <TableHead className="w-[1px] bg-card-header-foreground p-0" />
-
-              <TableHead className="text-center font-bold">DC 1X</TableHead>
-              <TableHead className="text-center font-bold">DC X2</TableHead>
-              <TableHead className="text-center font-bold">DC 12</TableHead>
-              <TableHead className="w-[1px] bg-card-header-foreground p-0" />
-
-              <TableHead className="text-center font-bold">Goal</TableHead>
-              <TableHead className="text-center font-bold">No Goal</TableHead>
-              <TableHead className="w-[1px] bg-card-header-foreground p-0" />
-
-              <TableHead className="text-center font-bold">U 2.5</TableHead>
-              <TableHead className="text-center font-bold">O 2.5</TableHead>
-
+              <TableHead className="w-[225px]"></TableHead>
+              {marketTabs
+                .find((tab) => tab.name === selectedTab)
+                ?.markets.map((market, index) => (
+                  <>
+                    {market.selections
+                      .flatMap(({ selection }) => selection)
+                      .map((option, i) => (
+                        <TableHead
+                          key={`${index}-${i}`}
+                          className="text-center font-bold"
+                        >
+                          {option.outcome} {getFloatFromString(market.name)}
+                        </TableHead>
+                      ))}
+                    {index <=
+                      market.selections.flatMap(({ selection }) => selection)
+                        .length && (
+                      <TableHead className="w-[1px] bg-border p-0"></TableHead>
+                    )}
+                  </>
+                ))}
               <TableHead></TableHead>
             </TableRow>
           </TableHeader>
@@ -100,140 +124,43 @@ export default function UpcomingRoundCard(props: {
                   .map((t) => t.name || '')
                   .join(' - ')
 
-                const mainMarket = match.markets.market.find(
-                  (m) => m.name === 'Esito finale 1X2',
-                )
-                const dcMarket = match.markets.market.find(
-                  (m) => m.name === 'Doppia Chance',
-                )
-                const underOverMarket = match.markets.market.find(
-                  (m) => m.name.trim() === 'Under\/Over 2.5',
-                )
-                const goalNoGoalMarket = match.markets.market.find(
-                  (m) => m.name === 'Gol no gol',
-                )
-
-                const marketOptions =
-                  mainMarket?.selections.flatMap(
-                    ({ selection }) => selection,
-                  ) || []
-
-                const dcMarketOptions =
-                  dcMarket?.selections.flatMap(({ selection }) => selection) ||
-                  []
-
-                const underOverOptions =
-                  underOverMarket?.selections.flatMap(
-                    ({ selection }) => selection,
-                  ) || []
-
-                const goalNoGoalOptions =
-                  goalNoGoalMarket?.selections.flatMap(
-                    ({ selection }) => selection,
-                  ) || []
-
                 return (
-                  <TableRow key={index} className="border-card-foreground">
-
-                    <TableCell className="h-[70px] w-[142px] p-0 text-center">
+                  <TableRow key={index} className="h-[70px] border-card-foreground">
+                    <TableCell className="p-0 text-center">
                       <span className="text-[16px] font-bold">{teamNames}</span>
                     </TableCell>
 
-                    {mainMarket ? (
-                      marketOptions.map((option, i) => (
-                        <TableCell
-                          key={i}
-                          className="h-[70px] w-[116px] px-[10px] text-center"
-                        >
-                          <BetEntryToggle
-                            matchStart={matchStart}
-                            round={props.round}
-                            teams={teamNames}
-                            marketName={mainMarket.name}
-                            option={option}
-                            className="h-[45px] w-[90px] text-[19px] font-semibold"
-                          />
-                        </TableCell>
-                      ))
-                    ) : (
-                      <TableCell colSpan={3} className="text-center">
-                        {t('no_odds')}
-                      </TableCell>
-                    )}
+                    {marketTabs
+                      .find((tab) => tab.name === selectedTab)
+                      ?.markets.map((market, index) => (
+                        <>
+                          {market.selections
+                            .flatMap(({ selection }) => selection)
+                            .map((option, i) => (
+                              <TableCell
+                                key={i}
+                                className="px-[10px] text-center"
+                              >
+                                <BetEntryToggle
+                                  matchStart={matchStart}
+                                  round={props.round}
+                                  teams={teamNames}
+                                  marketName={market.name}
+                                  option={option}
+                                  className="h-[45px] w-[90px] text-[19px] font-semibold"
+                                />
+                              </TableCell>
+                            ))}
+                          {index <=
+                            market.selections.flatMap(
+                              ({ selection }) => selection,
+                            ).length && (
+                            <TableCell className="w-[1px] bg-border p-0"></TableCell>
+                          )}
+                        </>
+                      ))}
 
-                    <TableCell className="w-[1px] bg-border p-0" />
-
-                    {dcMarket ? (
-                      dcMarketOptions.map((option, i) => (
-                        <TableCell
-                          key={i}
-                          className="h-[70px] w-[116px] px-[10px] text-center"
-                        >
-                          <BetEntryToggle
-                            matchStart={matchStart}
-                            round={props.round}
-                            teams={teamNames}
-                            marketName={dcMarket.name}
-                            option={option}
-                            className="h-[45px] w-[90px] text-[19px] font-semibold"
-                          />
-                        </TableCell>
-                      ))
-                    ) : (
-                      <TableCell colSpan={3} className="text-center">
-                        {t('no_odds')}
-                      </TableCell>
-                    )}
-
-                    <TableCell className="w-[1px] bg-border p-0" />
-
-                    {goalNoGoalMarket ? (
-                      goalNoGoalOptions.map((option, i) => (
-                        <TableCell
-                          key={i}
-                          className="h-[70px] w-[116px] px-[10px] text-center"
-                        >
-                          <BetEntryToggle
-                            matchStart={matchStart}
-                            round={props.round}
-                            teams={teamNames}
-                            marketName={goalNoGoalMarket.name}
-                            option={option}
-                            className="h-[45px] w-[90px] text-[19px] font-semibold"
-                          />
-                        </TableCell>
-                      ))
-                    ) : (
-                      <TableCell colSpan={2} className="text-center">
-                        {t('no_odds')}
-                      </TableCell>
-                    )}
-
-                    <TableCell className="w-[1px] bg-border p-0" />
-
-                    {underOverMarket ? (
-                      underOverOptions.map((option, i) => (
-                        <TableCell
-                          key={i}
-                          className="h-[70px] w-[116px] px-[10px] text-center"
-                        >
-                          <BetEntryToggle
-                            matchStart={matchStart}
-                            round={props.round}
-                            teams={teamNames}
-                            marketName={underOverMarket.name}
-                            option={option}
-                            className="h-[45px] w-[90px] text-[19px] font-semibold"
-                          />
-                        </TableCell>
-                      ))
-                    ) : (
-                      <TableCell colSpan={2} className="text-center">
-                        {t('no_odds')}
-                      </TableCell>
-                    )}
-
-                    <TableCell className="text-center">
+                    <TableCell className="text-right pr-4">
                       <Button
                         variant="action"
                         size="icon-lg"
