084cc8385079ae94b355d2efea4ba2eb9f371749
AndrewChop <amarantoandreagmail.com>
2025-07-15 18:10:46 +0200

    feat: refactor upcoming rounds to upcoming events and update related state management

--- src/app/retail/(calcio)/calcio/page.tsx
+++ src/app/retail/(calcio)/calcio/page.tsx
@@ -4,17 +4,20 @@ import Leaderboard from '@/retail-components/leaderboard'
 import MatchBettingOptions from '@/retail-components/match-betting-options'
 import SearchEventResults from '@/retail-components/search-event-results'
 import { ScrollArea } from '@/retail-components/ui/scroll-area'
+import { UpcomingEventsCarousel } from '@/retail-components/upcoming-events-carousel'
 import UpcomingRoundCard from '@/retail-components/upcoming-round-card'
-import { UpcomingRoundsCarousel } from '@/retail-components/upcoming-rounds-carousel'
 import { RootContext } from '@/retail-contexts/root-context'
-import { Market, UpcomingRound } from '@/retail-lib/types'
+import { Market, UpcomingEvent, UpcomingRound } from '@/retail-lib/types'
 import { useContext, useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
 export default function Home() {
   const { t } = useTranslation()
-  const { upcomingRounds, searchRoundResults, setSearchRoundResults } =
-    useContext(RootContext)
+  const {
+    upcomingEvents,
+    searchEventResults: searchRoundResults,
+    setSearchEventResults: setSearchRoundResults,
+  } = useContext(RootContext)
 
   const [matchBetOptions, setMatchBetOptions] = useState<{
     round: {
@@ -26,22 +29,26 @@ export default function Home() {
     markets: Market[]
   }>()
 
-  const [selectedRound, setSelectedRound] = useState<UpcomingRound>()
+  const [selectedEvent, setSelectedEvent] = useState<UpcomingEvent>()
 
   useEffect(() => {
-    if (!selectedRound && upcomingRounds && upcomingRounds.length > 0) {
-      setSelectedRound(upcomingRounds[0])
+    if (!selectedEvent && upcomingEvents && upcomingEvents.length > 0) {
+      setSelectedEvent(upcomingEvents[0])
     }
-  }, [upcomingRounds, selectedRound])
+  }, [upcomingEvents, selectedEvent])
+
+  useEffect(() => {
+    console.log('Search round results updated:', searchRoundResults)
+  }, [searchRoundResults])
 
   return (
     <div className="flex h-full flex-row overflow-hidden py-2">
       <div className="flex flex-col">
-        <div className="mx-2 flex h-16 w-[1500px] flex-row items-center justify-center bg-accent px-4">
-          <UpcomingRoundsCarousel
-            selectedRound={selectedRound}
-            setSelectedRound={(round) => {
-              setSelectedRound(round)
+        <div className="mx-2 flex h-[80px] w-[1500px] flex-row items-center justify-center bg-accent px-4">
+          <UpcomingEventsCarousel
+            selectedEvent={selectedEvent}
+            setSelectedEvent={(event) => {
+              setSelectedEvent(event)
               setSearchRoundResults(undefined)
             }}
           />
@@ -56,7 +63,7 @@ export default function Home() {
                   eventResults={searchRoundResults}
                   onClose={() => setSearchRoundResults(undefined)}
                 />
-              ) : selectedRound ? (
+              ) : selectedEvent ? (
                 matchBetOptions ? (
                   <MatchBettingOptions
                     round={matchBetOptions.round}
@@ -67,7 +74,7 @@ export default function Home() {
                 ) : (
                   <>
                     <UpcomingRoundCard
-                      round={selectedRound}
+                      round={selectedEvent.data as UpcomingRound}
                       viewMatchBettingOptions={setMatchBetOptions}
                     />
                     <Leaderboard />
