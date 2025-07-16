3f4fe17c134e4bd206470ffb654ecc5e8f5e6e1a
AndrewChop <amarantoandreagmail.com>
2025-07-16 15:37:11 +0200

    feat: enhance UpcomingEvent and UpcomingRace types with additional properties for odds and latecomers

--- src/retail-lib/types.ts
+++ src/retail-lib/types.ts
@@ -87,16 +87,54 @@ export type UpcomingMatch = {
 
 export type UpcomingEvent = {
   id: number
+  extId?: string
   discipline: Discipline
   name: string
   startTime: string
   time: Date
   duration: number
-  data: UpcomingRound | UpcomingRace
+  data?: UpcomingRound | UpcomingRace
 }
 
 export type UpcomingRace = {
   id: number
+  odds: {
+    winner: Record<string, string>
+    placed: Record<string, string>
+    show: Record<string, string>
+    exacta: Record<string, Record<string, string>>
+    quinella: Record<string, Record<string, string>>
+    trifecta: Record<string, Record<string, Record<string, string>>>
+    boxedtrifecta: Record<string, Record<string, Record<string, string>>>
+    evenodd: {
+      even: string
+      odd: string
+    }
+    underover: {
+      under: string
+      over: string
+    }
+  }
+  latecomers: {
+    winner: {
+      racers: number[]
+      delay: number
+    }
+    exacta: {
+      racers: number[]
+      delay: number
+    }
+    trifecta: {
+      racers: number[]
+      delay: number
+    }
+  }
+  racers: Array<{
+    number: number
+    name: string
+    history: number[]
+    performance: number
+  }>
 }
 
 export type UpcomingRound = {
@@ -142,12 +180,12 @@ export type RaceResult = {
 }
 
 export type Bet = {
-  round: {
+  event: {
     name: string
     number: number
     startingAt: Date
   }
-  teams: string
+  competitor: string
   option: Selection
 }
 
