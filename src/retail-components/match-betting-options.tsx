08edd62b2663130dbbb1a98b9a9d157e869aff4b
expl0r3rgu1 <salvatore.addimando@startegois.com>
2025-07-17 16:22:18 +0200

    refactor: improve layout and structure of MatchBettingOptions component

--- src/retail-components/match-betting-options.tsx
+++ src/retail-components/match-betting-options.tsx
@@ -34,88 +34,83 @@ export default function MatchBettingOptions(props: {
   })
 
   return (
-    <div className="flex h-full w-full flex-col gap-4 overflow-hidden">
-      <div ref={ref}>
-        <div className="sticky top-0 z-10 flex h-16 flex-row items-center justify-between bg-accent text-accent-foreground">
-          <div className="flex flex-row items-center gap-2">
-            <Button
-              className="ml-3 rounded-[8px] bg-tertiary text-tertiary-foreground hover:bg-tertiary/70"
-              onClick={props.close}
-              size="icon-lg"
-            >
-              <ChevronsLeftIcon style={{ scale: 2 }} />
-            </Button>
-            <span className="text-[24px]">
-              {props.round.name} {t('round')} {props.round.number} /
-            </span>
-            <span className="text-[20px] font-semibold">{props.teams}</span>
-          </div>
-          <span className="mr-5 text-[20px]">
-            {format(props.round.startingAt, 'HH:mm')}
+    <div ref={ref} className="flex h-full w-full flex-col gap-4">
+      <div className="flex flex-row items-center justify-between bg-accent p-3 text-accent-foreground">
+        <div className="flex flex-row items-center gap-2">
+          <Button
+            className="rounded-[8px] bg-tertiary text-tertiary-foreground hover:bg-tertiary/70"
+            onClick={props.close}
+            size="icon-lg"
+          >
+            <ChevronsLeftIcon style={{ scale: 2 }} />
+          </Button>
+          <span className="text-[24px]">
+            {props.round.name} {t('round')} {props.round.number} /
           </span>
+          <span className="text-[20px] font-semibold">{props.teams}</span>
+        </div>
+        <span className="text-[20px]">
+          {format(props.round.startingAt, 'HH:mm')}
+        </span>
+      </div>
+
+      <div className="flex flex-col gap-4 overflow-y-auto">
+        <div className="flex flex-row items-start justify-between">
+          <MatchResultCard
+            matchResult={{
+              teams: props.teams,
+              score1: 1,
+              score2: 2,
+            }}
+          />
+          <Separator
+            orientation="vertical"
+            className="mx-4 h-auto bg-border"
+            style={{ minHeight: '200px', width: '1px' }}
+          />
+          <MatchStatistics
+            match={{
+              teams: props.teams,
+              probabilities: [10, 50, 40],
+              startTime: props.round.startingAt,
+            }}
+          />
         </div>
 
-        <div
-          className="flex flex-col gap-4 overflow-y-auto pt-12"
-          style={{ height: 'calc(100% - 64px)' }}
-        >
-          <div className="flex flex-row items-start justify-between">
-            <MatchResultCard
-              matchResult={{
-                teams: props.teams,
-                score1: 1,
-                score2: 2,
-              }}
-            />
-            <Separator
-              orientation="vertical"
-              className="mx-4 h-auto bg-border"
-              style={{ minHeight: '200px', width: '1px' }}
-            />
-            <MatchStatistics
-              match={{
-                teams: props.teams,
-                probabilities: [10, 50, 40],
-                startTime: props.round.startingAt,
-              }}
-            />
-          </div>
-
-          <div className="pb-20">
-            <Accordion type="multiple" className="space-y-2">
-              {props.markets.map((market) => (
-                <AccordionItem
-                  key={market.name}
-                  value={market.name}
-                  className="text-accent-foreground"
-                >
-                  <AccordionTrigger className="h-12 text-[16px] font-bold">
-                    {market.name.toUpperCase()}
-                    <ChevronDown className="h-6 w-6 shrink-0 text-card transition-transform duration-200" />
-                  </AccordionTrigger>
-                  <AccordionContent>
-                    <div className="grid grid-cols-3 items-center justify-between gap-4 px-2">
-                      {market.selections[0].selection.map((option) => (
-                        <BetEntryToggle
-                          key={option.outcome}
-                          matchStart={props.round.startingAt}
-                          marketName={market.name}
-                          option={option}
-                          round={{
-                            scheduleName: props.round.name,
-                            scheduleId: props.round.number,
-                          }}
-                          teams={props.teams}
-                          showOutcome
-                          className="h-[45px] w-full font-semibold"
-                        />
-                      ))}
-                    </div>
-                  </AccordionContent>
-                </AccordionItem>
-              ))}
-            </Accordion>
-          </div>
+        <div className="pb-20">
+          <Accordion type="multiple" className="space-y-2">
+            {props.markets.map((market) => (
+              <AccordionItem
+                key={market.name}
+                value={market.name}
+                className="text-accent-foreground"
+              >
+                <AccordionTrigger className="h-12 text-[16px] font-bold">
+                  {market.name.toUpperCase()}
+                  <ChevronDown className="h-6 w-6 shrink-0 text-card transition-transform duration-200" />
+                </AccordionTrigger>
+                <AccordionContent>
+                  <div className="grid grid-cols-3 items-center justify-between gap-4 px-2">
+                    {market.selections[0].selection.map((option) => (
+                      <BetEntryToggle
+                        key={option.outcome}
+                        matchStart={props.round.startingAt}
+                        marketName={market.name}
+                        option={option}
+                        round={{
+                          scheduleName: props.round.name,
+                          scheduleId: props.round.number,
+                        }}
+                        teams={props.teams}
+                        showOutcome
+                        className="h-[45px] w-full font-semibold"
+                      />
+                    ))}
+                  </div>
+                </AccordionContent>
+              </AccordionItem>
+            ))}
+          </Accordion>
         </div>
       </div>
     </div>
