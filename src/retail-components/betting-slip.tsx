bbd7cba463bad6d3613ff485f43b35b62c963c4c
AndrewChop <amarantoandreagmail.com>
2025-07-22 12:41:36 +0200

    feat: implement EventBets component for better event handling in BettingSlip

--- src/retail-components/betting-slip.tsx
+++ src/retail-components/betting-slip.tsx
@@ -1,19 +1,18 @@
 'use client'
 
-import { Badge } from '@/retail-components/ui/badge'
 import { Button } from '@/retail-components/ui/button'
 import { Card, CardContent, CardFooter } from '@/retail-components/ui/card'
 import { ScrollArea } from '@/retail-components/ui/scroll-area'
 import { BetsContext } from '@/retail-contexts/bets-context'
 import { generateSystemGroups } from '@/retail-lib/system-bets'
 import { BetEntry, SubmittedTicket, SystemGroup } from '@/retail-lib/types'
-import { getTimeDistanceFromNow } from '@/retail-lib/utils'
-import { CircleXIcon, RotateCcwIcon } from 'lucide-react'
-import Image from 'next/image'
+import { RotateCcwIcon } from 'lucide-react'
 import { useContext, useEffect, useMemo, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import FastBet from './fast-bet'
+import EventBets from './event-bets'
 import StakeInputDialog from './stake-input-dialog'
+import { Separator } from './ui/separator'
 import {
   Table,
   TableBody,
@@ -23,21 +22,12 @@ import {
   TableHeader,
   TableRow,
 } from './ui/table'
-import { Separator } from './ui/separator'
-import { Checkbox } from './ui/checkbox'
-import { format } from 'date-fns'
 
-type BetMode = 'SINGLE' | 'MULTIPLE' | 'SYSTEM'
+export type BetMode = 'SINGLE' | 'MULTIPLE' | 'SYSTEM'
 
 export default function BettingSlip() {
-  const {
-    betEntries,
-    removeBet,
-    removeMatchBets,
-    toggleMatchBetsFixed,
-    removeAllBets,
-    restoreLastSubmittedTicket,
-  } = useContext(BetsContext)
+  const { betEntries, removeAllBets, restoreLastSubmittedTicket } =
+    useContext(BetsContext)
 
   const betsByMatch = useMemo(() => {
     return betEntries.reduce(
@@ -137,91 +127,12 @@ export default function BettingSlip() {
           <ScrollArea className="h-full">
             <ul className="flex flex-col gap-1 bg-background">
               {Object.entries(betsByMatch).map(([matchKey, matchBets]) => (
-                <li key={matchKey}>
-                  <div className="flex flex-col gap-1 border border-betSlip-foreground p-1">
-                    <div className="flex flex-row justify-between">
-                      <div
-                        className={
-                          betMode === 'SYSTEM' ? 'visible' : 'invisible'
-                        }
-                      >
-                        <div className="flex flex-row items-center gap-2 pl-1">
-                          <Checkbox
-                            checked={matchBets[0].fixed}
-                            onCheckedChange={() =>
-                              toggleMatchBetsFixed(matchKey)
-                            }
-                          />
-                          <span className="text-[12px]">{t('fixed')}</span>
-                        </div>
-                      </div>
-                      <Button
-                        variant="ghost"
-                        className="group size-7 hover:text-tertiary-foreground"
-                        size="icon"
-                        onClick={() => removeMatchBets(matchKey)}
-                      >
-                        <Image
-                          src="/bin.svg"
-                          alt="Bin"
-                          width={40}
-                          height={20}
-                          className="size-5 object-contain group-hover:brightness-0 group-hover:invert"
-                        />
-                      </Button>
-                    </div>
-
-                    <div className="flex items-center justify-between">
-                      <span className="text-[16px] font-semibold">
-                        {t('football')}
-                      </span>
-                      <div className="flex items-center gap-2">
-                        <span className="text-[16px] font-bold">
-                          {format(matchBets[0].bet.event.startingAt, 'HH:mm')}
-                        </span>
-                        <Badge className="bg-accent text-[16px]">
-                          {getTimeDistanceFromNow(
-                            new Date(matchBets[0].bet.event.startingAt),
-                          )}
-                        </Badge>
-                      </div>
-                    </div>
-
-                    <span className="text-[16px]">
-                      {matchBets[0].bet.competitor}
-                    </span>
-                  </div>
-
-                  <div className="border border-betSlip-foreground bg-primary-foreground p-1">
-                    {matchBets.map((betEntry) => (
-                      <div
-                        key={betEntry.id}
-                        className="flex items-center justify-between text-sm"
-                      >
-                        <span className="text-[14px]">{betEntry.market}</span>
-                        <span className="text-[14px] font-bold">
-                          {betEntry.bet.option.outcome}
-                        </span>
-                        <span className="text-[14px]">
-                          {betEntry.bet.option.decPrice.toFixed(2)}
-                        </span>
-                        <Button
-                          variant="ghost"
-                          size="icon"
-                          onClick={() =>
-                            removeBet(
-                              betEntry.market,
-                              betEntry.bet.option,
-                              betEntry.bet.competitor,
-                            )
-                          }
-                        >
-                          <CircleXIcon style={{ scale: 1.5 }} />
-                        </Button>
-                      </div>
-                    ))}
-                  </div>
-                </li>
+                <EventBets
+                  key={matchKey}
+                  betMode={betMode}
+                  eventKey={matchKey}
+                  eventBets={matchBets}
+                />
               ))}
             </ul>
           </ScrollArea>
