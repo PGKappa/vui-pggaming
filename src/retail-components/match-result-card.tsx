c34540d22de1491d70ff7856729548a307250412
AndrewChop <amarantoandreagmail.com>
2025-07-27 20:49:06 +0200

    feat: reduced spaces for match result and statistics labels

--- src/retail-components/match-result-card.tsx
+++ src/retail-components/match-result-card.tsx
@@ -1,15 +1,16 @@
 import { MatchResult } from '@/retail-lib/types'
+import { t } from 'i18next'
 
 export default function MatchResultCard(props: { matchResult?: MatchResult }) {
   if (!props.matchResult) {
     return (
       <div className="w-full">
         <div className="text-center">
-          <h4 className="text-[22px] font-extrabold text-secondary">RESULT</h4>
+          <h4 className="text-[22px] font-extrabold text-secondary">
+            {t('result').toUpperCase()}
+          </h4>
         </div>
-        <div className="py-4 text-center">
-          <p>No match results</p>
-        </div>
+        <div className="py-4 text-center">{t('no_match_results')}</div>
       </div>
     )
   }
@@ -18,10 +19,12 @@ export default function MatchResultCard(props: { matchResult?: MatchResult }) {
   return (
     <div className="flex w-full flex-col items-center justify-start gap-10">
       <div className="text-center">
-        <h4 className="text-[22px] font-extrabold text-secondary">RESULT</h4>
+        <h4 className="text-[22px] font-extrabold text-secondary">
+          {t('result').toUpperCase()}
+        </h4>
       </div>
 
-      <div className="flex w-full flex-row items-center justify-around">
+      <div className="flex w-full flex-row items-center justify-center gap-52">
         <div className="flex flex-row gap-5">
           <div className="flex flex-col items-center gap-1">
             <div className="h-5 w-5 rounded-full bg-chart-3"></div>
