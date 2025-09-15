0a49cfe2f14fcd25fc7778ea539b824c51046407
AndrewChop <amarantoandreagmail.com>
2025-09-15 16:05:47 +0200

    fix: adjust height of Skeleton component in SkeletonRoundCard

--- src/retail-components/skeleton-round-card.tsx
+++ src/retail-components/skeleton-round-card.tsx
@@ -4,7 +4,7 @@ export default function SkeletonRoundCard() {
   return (
     <div className="space-y-4">
       <Skeleton className="h-16 w-full rounded-lg" />
-      <Skeleton className="h-[740px] w-full rounded-lg" />
+      <Skeleton className="h-[700px] w-full rounded-lg" />
       <Skeleton className="h-12 w-full rounded-lg" />
     </div>
   )
