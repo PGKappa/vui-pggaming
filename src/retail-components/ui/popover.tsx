6fc2dc7c2278d0b4d3a33fd1f6d08def9cfbe8bb
AndrewChop <amarantoandreagmail.com>
2025-10-01 17:06:35 +0200

    fix: update import paths from virtual-lib to retail-lib for consistency

--- src/retail-components/ui/popover.tsx
+++ src/retail-components/ui/popover.tsx
@@ -3,7 +3,7 @@
 import * as React from 'react'
 import * as PopoverPrimitive from '@radix-ui/react-popover'
 
-import { cn } from '@/virtual-lib/utils'
+import { cn } from '@/retail-lib/utils'
 
 const Popover = PopoverPrimitive.Root
 
