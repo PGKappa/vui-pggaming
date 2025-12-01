098ad77b8e5a3dcb86a44ae142ba592287b4bc20
Andrea Amaranto <amarantoandrea@gmail.com>
2025-12-01 12:21:24 +0100

    Enhance UpcomingEventsCarousel to support DOGS8 discipline and improve layout consistency

--- src/retail-components/upcoming-events-carousel.tsx
+++ src/retail-components/upcoming-events-carousel.tsx
@@ -23,8 +23,12 @@ export function UpcomingEventsCarousel(props: {
   const { t } = useTranslation()
 
   const disciplines = useMemo(() => {
+    if (typeof window === 'undefined') return []
+
     const path = window.location.pathname
-    if (path.includes('dogs-horses')) {
+    if (path.includes('dogs8')) {
+      return [Discipline.DOGS8]
+    } else if (path.includes('dogs-horses')) {
       return [Discipline.DOGS, Discipline.HORSES]
     } else if (path.includes('dogs')) {
       return [Discipline.DOGS]
@@ -38,18 +42,18 @@ export function UpcomingEventsCarousel(props: {
   const filteredAndSortedEvents = useMemo(() => {
     return upcomingEvents
       ? upcomingEvents
-        .filter((event) => disciplines.includes(event.discipline))
-        .sort((a, b) => {
-          const timeA = new Date(a.time).getTime()
-          const timeB = new Date(b.time).getTime()
-          return timeA - timeB
-        })
+          .filter((event) => disciplines.includes(event.discipline))
+          .sort((a, b) => {
+            const timeA = new Date(a.time).getTime()
+            const timeB = new Date(b.time).getTime()
+            return timeA - timeB
+          })
       : []
   }, [upcomingEvents, disciplines])
 
   return (
     <Carousel className="w-[1370px]">
-      <CarouselContent className="bg-white relative left-[26px]">
+      <CarouselContent className="relative left-[26px] bg-white">
         {isLoadingEvents ? (
           // Show skeleton loading
           Array.from({ length: 6 }).map((_, index) => (
@@ -105,11 +109,12 @@ function UpcomingEventItem(props: {
 
   return (
     <CarouselItem
-      className={`basis-1/7 flex h-[65px] w-[186px] cursor-pointer flex-row items-center  justify-center gap-3 px-3 py-2 text-[15px] ${event.id === props.selectedEvent?.id &&
+      className={`basis-1/7 flex h-[65px] w-[186px] cursor-pointer flex-row items-center justify-center gap-3 px-3 py-2 text-[15px] ${
+        event.id === props.selectedEvent?.id &&
         event.discipline === props.selectedEvent?.discipline
-        ? 'bg-[hsl(211deg_65%_37%_/_.9)] text-tertiary-foreground'
-        : 'bg-secondary text-secondary-foreground'
-        }`}
+          ? 'bg-[hsl(211deg_65%_37%_/_.9)] text-tertiary-foreground'
+          : 'bg-secondary text-secondary-foreground'
+      }`}
       onClick={() => {
         props.setSelectedEvent(event)
       }}
@@ -130,19 +135,22 @@ function UpcomingEventItem(props: {
         />
       </div>
       <div className="flex flex-col items-start pr-[1px]">
-        <span className='text-[15] relative top-[4px] font-semibold'>
+        <span className="relative top-[4px] font-semibold text-[15]">
           {event.discipline === 'SOCCER'
             ? event.name
             : `${event.name} ${t('racing')}`}
         </span>
 
-        <span className="text-[14px] font-normal relative bottom-[0px]">
+        <span className="relative bottom-[0px] text-[14px] font-normal">
           {event.discipline === 'SOCCER' ? t('round') : t('event')} {event.id}
         </span>
         <div className="flex flex-row gap-2">
-          <span className="font-semibold text-[14px] relative bottom-[2px]">{event.startTime}</span>
-          <span className="text-[14px] font-semibold  absolute pl-[45px] py-0 bottom-[3px] text-[#99a6b1]">{timeToEventStart}</span>
-          
+          <span className="relative bottom-[2px] text-[14px] font-semibold">
+            {event.startTime}
+          </span>
+          <span className="absolute bottom-[3px] py-0 pl-[45px] text-[14px] font-semibold text-[#99a6b1]">
+            {timeToEventStart}
+          </span>
         </div>
       </div>
     </CarouselItem>
