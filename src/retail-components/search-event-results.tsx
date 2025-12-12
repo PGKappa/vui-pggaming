94815616f198e01230216e80f16cbb5bc0ba0071
Andrea Amaranto <amarantoandrea@gmail.com>
2025-12-12 18:29:54 +0100

    Fix track display logic in SearchEventResults to default to '6' when track is undefined

--- src/retail-components/search-event-results.tsx
+++ src/retail-components/search-event-results.tsx
@@ -572,18 +572,18 @@ export default function SearchEventResults() {
                                 </span>
 
                                 {/* Track */}
-                                {eventResult.track && (
+                                {(eventResult.track || '6') && (
                                   <span className="whitespace-nowrap border-l border-l-white pl-4">
                                     {(() => {
                                       // Se track è un numero (6 o 8), usa la translation key
                                       const trackNum = parseInt(
-                                        eventResult.track,
+                                        eventResult.track || '6',
                                       )
                                       if (!isNaN(trackNum)) {
                                         return t(`track_${trackNum}`)
                                       }
                                       // Altrimenti mostra il valore diretto (track_name)
-                                      return eventResult.track
+                                      return eventResult.track || '6'
                                     })()}
                                   </span>
                                 )}
