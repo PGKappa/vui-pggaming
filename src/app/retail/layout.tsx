84543778add1813bf6c21f3d2f5b648277b552cd
Andrea Amaranto <amarantoandrea@gmail.com>
2026-01-19 12:02:34 +0100

    Fix import order in layout component and adjust CSS import placement

--- src/app/retail/layout.tsx
+++ src/app/retail/layout.tsx
@@ -1,8 +1,8 @@
 'use client'
 
+import InactivityBridge from '@/retail-components/inactivity-bridge'
 import Navbar from '@/retail-components/navbar'
 import { Toaster } from '@/retail-components/ui/sonner'
-import InactivityBridge from '@/retail-components/inactivity-bridge'
 import ZoomBlocker from '@/retail-components/zoom-blocker'
 import BetsContextProvider from '@/retail-contexts/bets-context'
 import CashierContextProvider from '@/retail-contexts/cashier-context'
@@ -13,8 +13,8 @@ import { Inter } from 'next/font/google'
 import { usePathname } from 'next/navigation'
 import { useContext } from 'react'
 import { useTranslation } from 'react-i18next'
-import '../globals.css'
 import '../../retail-lib/i18n'
+import '../globals.css'
 
 const inter = Inter({
   variable: '--font-inter',
