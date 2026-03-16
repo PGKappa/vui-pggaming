c05584e0a37f50a4512934ca82d3d74451bb56f1
gensal77 <gennaro.salvati.03@gmail.com>
2026-03-16 17:54:54 +0100

    correzione scroll

--- src/app/retail/(cani)/dogs/page.tsx
+++ src/app/retail/(cani)/dogs/page.tsx
@@ -79,7 +79,7 @@ export default function Home() {
   }, [selectedEvent, upcomingEvents])
 
   return (
-    <div className="relative bottom-[5px] flex h-full flex-row overflow-hidden">
+    <div className="relative bottom-[5px] flex h-full min-w-[1200px] flex-row overflow-hidden">
       {/* LEFT COLUMN - si allarga/stringe in base alla risoluzione */}
       <div className="flex min-w-0 flex-1 flex-col overflow-hidden">
         <div className="bg-betslip flex h-[99px] w-full flex-row items-center justify-center pb-[2px] pr-2">
