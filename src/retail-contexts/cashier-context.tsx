2e040d0e9bf242168106ae6af61aeb71e7eec725
Andrea Amaranto <amarantoandrea@gmail.com>
2026-04-07 11:54:02 +0200

    Update dependency in useEffect to include translation function

--- src/retail-contexts/cashier-context.tsx
+++ src/retail-contexts/cashier-context.tsx
@@ -342,7 +342,7 @@ export default function CashierContextProvider(props: {
         setIsLoading(false)
       }
     }
-  }, [])
+  }, [t])
 
   // Fetch cashier data (solo una volta, poi cache)
   useEffect(() => {
