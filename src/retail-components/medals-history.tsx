4abb5edbd303c56374c7ee0b9eb6db30e362c410
gensal77 <gennaro.salvati.03@gmail.com>
2026-07-25 17:18:42 +0200

    refactor: enhance responsiveness in UpcomingRaceCard and MedalsHistory components by adding viewport detection and conditional rendering for performance and history display

--- src/retail-components/medals-history.tsx
+++ src/retail-components/medals-history.tsx
@@ -22,7 +22,7 @@ export default function MedalsHistory(props: { history: number[] }) {
         return (
           <div
             key={idx}
-            className="flex h-8 w-6 shrink-0 items-center justify-center text-[20px] text-black min-[1600px]:w-[26px] min-[1920px]:w-7"
+            className="flex h-8 w-6 shrink-0 items-center justify-center text-[20px] font-normal text-black min-[1600px]:w-[26px] min-[1920px]:w-7"
           >
             X
           </div>
